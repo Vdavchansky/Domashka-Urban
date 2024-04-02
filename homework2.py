97dde2c1553ccda5d300815a36bfcaa261e4c6e1
Vdavchansky <51847880+Vdavchansky@users.noreply.github.com>
2024-04-02 07:13:41 +0600

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,8 @@
+Name = "Alex"
+Age = 21
+New_age = Age+1
+Is_student = True
+print("Name:" + Name)
+print("Age:" + str(Age))
+print("New_age:" + str(New_age))
+print("Is_student:" + str(Is_student))
